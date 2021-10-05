08dbebf4d3689169420270f28f1ef390ce2c0a38
Raymond Gauthier <jraygauthier@gmail.com>
2021-10-05 18:29:49 -0400

    Successfully cross compile hello world to windows
    Using mingw_w64 (with a slight patch).

---
+++ hello-static-rustup-target-windows/README.md
@@ -0,0 +1,44 @@
+Readme
+======
+
+## Build targetting windows platform via rustup / rustc
+
+### Statically linked to libc
+
+Required nix env:
+
+```bash
+$ nix-shell -I nixpkgs=../../nixpkgs_root/
+# ..
+```
+
+```bash
+$ rustup target add x86_64-pc-windows-gnu
+
+$ rustc --target x86_64-pc-windows-gnu hello.rs
+
+$ file ./hello.exe 
+./hello.exe: PE32+ executable (console) x86-64, for MS Windows
+```
+
+from `cmd.exe` on target windows device:
+
+```bash
+$ hello.exe
+Hello World!
+```
+
+
+## References
+
+ -  [Beginner’s guide to cross compilation in Nixpkgs](https://matthewbauer.us/blog/beginners-guide-to-cross.html)
+
+ -  [Cross Compile Rust for Windows - Learn - NixOS Discourse](https://discourse.nixos.org/t/cross-compile-rust-for-windows/9582/4)
+
+ -  [Demote Windows MinGW targets to lower tiers or re-enable their tests - tools and infrastructure - Rust Internals](https://internals.rust-lang.org/t/demote-windows-mingw-targets-to-lower-tiers-or-re-enable-their-tests/10536/13)
+
+
+ -  [x86_64-pc-windows-gnu target failing · Issue #7 · nix-community/fenix](https://github.com/nix-community/fenix/issues/7)
+
+ -  [x86_64-pc-windows-gnu target failing · Issue #7 · nix-community/fenix](https://github.com/nix-community/fenix/issues/7)
+
---
+++ hello-static-rustup-target-windows/shell.nix
@@ -0,0 +1,62 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  pkgs-cross-mingw = import pkgs.path {
+    crossSystem = {
+        config = "x86_64-w64-mingw32";
+      };
+  };
+in
+
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    llvmPackages_latest.llvm
+    llvmPackages_latest.bintools
+    zlib.out
+    rustup
+    xorriso
+    grub2
+    qemu
+    llvmPackages_latest.lld
+    python3
+
+    pkgs-cross-mingw.stdenv.cc
+    pkgs-cross-mingw.windows.mingw_w64
+    (pkgs-cross-mingw.windows.mingw_w64_pthreads.overrideAttrs (oldAttrs: {
+      # TODO: Remove once / if changed successfully upstreamed.
+      configureFlags = (oldAttrs.configureFlags or []) ++ [
+        # Rustc require 'libpthread.a' when targeting 'x86_64-pc-windows-gnu'.
+        # Enabling this makes it work out of the box instead of failing.
+        "--enable-static"
+      ];
+    }))
+  ];
+  # Avoid polluting home dir with local project stuff.
+  RUSTUP_HOME = toString ./.rustup;
+  CARGO_HOME = toString ./.cargo;
+  RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
+  # https://github.com/rust-lang/rust-bindgen#environment-variables
+  LIBCLANG_PATH= pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+  HISTFILE=toString ./.history;
+  shellHook = ''
+    export PATH=$PATH:${CARGO_HOME}/bin
+    export PATH=$PATH:${RUSTUP_HOME}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+    '';
+  # Add libvmi precompiled library to rustc search path
+  RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+    pkgs.libvmi
+  ]);
+  # Add libvmi, glibc, clang, glib headers to bindgen search path
+  BINDGEN_EXTRA_CLANG_ARGS =
+  # Includes with normal include path
+  (builtins.map (a: ''-I"${a}/include"'') [
+    pkgs.libvmi
+    pkgs.glibc.dev
+  ])
+  # Includes with special directory paths
+  ++ [
+    ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+    ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+    ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+  ];
+
+}
